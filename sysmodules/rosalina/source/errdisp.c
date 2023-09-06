dfa8622c0424d29fe9dcd2e689fc53bb0e45e691
M4nueleZ <manuelez2424@gmail.com>
2023-09-06 23:41:32 +0200

    Traduzione (10)

--- sysmodules/rosalina/source/errdisp.c
+++ sysmodules/rosalina/source/errdisp.c
@@ -141,11 +141,11 @@ static int ERRF_FormatRegisterDump(char *out, const ERRF_ExceptionData *exceptio
 static int ERRF_FormatGenericInfo(char *out, const ERRF_FatalErrInfo *info)
 {
     static const char *types[] = {
-        "generic", "corrupted", "card removed", "exception", "result failure", "generic (log only)", "invalid"
+        "generico", "corruzione", "scheda rimossa", "eccezione", "risultato fallimento", "generico (solo log)", "non valido"
     };
 
     static const char *exceptionTypes[] = {
-        "prefetch abort", "data abort", "undefined instruction", "VFP", "invalid"
+        "interruzione caricamento", "interruzione dati", "istruzione non definita", "VFP", "non valido"
     };
 
     const char *type = (u32)info->type > (u32)ERRF_ERRTYPE_LOG_ONLY ? types[6] : types[(u32)info->type];
@@ -159,12 +159,12 @@ static int ERRF_FormatGenericInfo(char *out, const ERRF_FatalErrInfo *info)
         const char *exceptionType = (u32) info->data.exception_data.excep.type > (u32)ERRF_EXCEPTION_VFP ?
                                     exceptionTypes[4] : exceptionTypes[(u32)info->data.exception_data.excep.type];
 
-        out += sprintf(out, "Error type:       exception (%s)\n", exceptionType);
+        out += sprintf(out, "Tipo errore:       eccezione (%s)\n", exceptionType);
     }
     else
-        out += sprintf(out, "Error type:       %s\n", type);
+        out += sprintf(out, "Tipo errore:       %s\n", type);
 
-    out += sprintf(out, "\nProcess ID:       %lu\n", info->procId);
+    out += sprintf(out, "\nID processo:       %lu\n", info->procId);
 
     res = svcOpenProcess(&processHandle, info->procId);
     if(R_SUCCEEDED(res))
@@ -174,8 +174,8 @@ static int ERRF_FormatGenericInfo(char *out, const ERRF_FatalErrInfo *info)
         svcGetProcessInfo((s64 *)name, processHandle, 0x10000);
         svcGetProcessInfo((s64 *)&titleId, processHandle, 0x10001);
         svcCloseHandle(processHandle);
-        out += sprintf(out, "Process name:     %s\n", name);
-        out += sprintf(out, "Process title ID: %016llx\n", titleId);
+        out += sprintf(out, "Nome processo:     %s\n", name);
+        out += sprintf(out, "Title ID processo: %016llx\n", titleId);
     }
 
     out += sprintf(out, "\n");
@@ -193,27 +193,27 @@ static int ERRF_FormatError(char *out, const ERRF_FatalErrInfo *info, bool isLog
         u64 timeNow = osGetTime();
         u64 timeAtBoot = timeNow - (1000 * svcGetSystemTick() / SYSCLOCK_ARM11);
         dateTimeToString(dateTimeStr, timeNow, false);
-        out += sprintf(out, "Reported on:      %s\n", dateTimeStr);
+        out += sprintf(out, "Segnalato il:      %s\n", dateTimeStr);
         dateTimeToString(dateTimeStr, timeAtBoot, false);
-        out += sprintf(out, "System booted on: %s\n\n", dateTimeStr);
+        out += sprintf(out, "Sistema riavviato il: %s\n\n", dateTimeStr);
 
     }
     switch (info->type)
     {
         case ERRF_ERRTYPE_NAND_DAMAGED:
-            out += sprintf(out, "The NAND chip has been damaged.\n");
+            out += sprintf(out, "Il chip NAND e' stato danneggiato.\n");
             break;
         case ERRF_ERRTYPE_CARD_REMOVED:
         {
-            const char *medium = R_MODULE(info->resCode) == RM_SDMC ? "SD card" : "cartridge";
-            out += sprintf(out, "The %s was removed.\n", medium);
+            const char *medium = R_MODULE(info->resCode) == RM_SDMC ? "Scheda SD" : "cartuccia";
+            out += sprintf(out, "La %s e' stata rimossa.\n", medium);
             break;
         }
         case ERRF_ERRTYPE_GENERIC:
         case ERRF_ERRTYPE_LOG_ONLY:
             out += ERRF_FormatGenericInfo(out, info);
-            out += sprintf(out, "Address:          0x%08lx\n", info->pcAddr);
-            out += sprintf(out, "Error code:       0x%08lx\n", info->resCode);
+            out += sprintf(out, "Indirizzo:          0x%08lx\n", info->pcAddr);
+            out += sprintf(out, "Codice errore:       0x%08lx\n", info->resCode);
             break;
         case ERRF_ERRTYPE_EXCEPTION:
             out += ERRF_FormatGenericInfo(out, info);
@@ -222,11 +222,11 @@ static int ERRF_FormatError(char *out, const ERRF_FatalErrInfo *info, bool isLog
             break;
         case ERRF_ERRTYPE_FAILURE:
             out += ERRF_FormatGenericInfo(out, info);
-            out += sprintf(out, "Error code:       0x%08lx\n", info->resCode);
-            out += sprintf(out, "Reason:           %.96s\n", info->data.failure_mesg);
+            out += sprintf(out, "Codice errore:       0x%08lx\n", info->resCode);
+            out += sprintf(out, "Motivo:           %.96s\n", info->data.failure_mesg);
             break;
         default:
-            out += sprintf(out, "Invalid fatal error data.\n");
+            out += sprintf(out, "Dati errore irreversibile non validi.\n");
     }
 
     // We might not always have enough space to display this on screen, so keep it to the log file
@@ -242,14 +242,14 @@ static void ERRF_DisplayError(ERRF_FatalErrInfo *info)
 {
     Draw_Lock();
 
-    u32 posY = Draw_DrawString(10, 10, COLOR_RED, "An error occurred (ErrDisp)");
+    u32 posY = Draw_DrawString(10, 10, COLOR_RED, "Si e' verificato un errore (ErrDisp)");
     char buf[0x400];
 
     ERRF_FormatError(buf, info, false);
     posY = posY < 30 ? 30 : posY;
 
     posY = Draw_DrawString(10, posY, COLOR_WHITE, buf);
-    posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "Press any button to reboot.");
+    posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "Premi un pulsante per riavviare.");
 
     Draw_FlushFramebuffer();
     Draw_Unlock();
--- sysmodules/rosalina/source/menus.c
+++ sysmodules/rosalina/source/menus.c
@@ -46,21 +46,21 @@
 Menu rosalinaMenu = {
     "Rosalina menu",
     {
-        { "Take screenshot", METHOD, .method = &RosalinaMenu_TakeScreenshot },
-        { "Change screen brightness", METHOD, .method = &RosalinaMenu_ChangeScreenBrightness },
+        { "Effettua screenshot", METHOD, .method = &RosalinaMenu_TakeScreenshot },
+        { "Imposta luminosita", METHOD, .method = &RosalinaMenu_ChangeScreenBrightness },
         { "Cheats...", METHOD, .method = &RosalinaMenu_Cheats },
         { "", METHOD, .method = PluginLoader__MenuCallback},
-        { "Process list", METHOD, .method = &RosalinaMenu_ProcessList },
-        { "Debugger options...", MENU, .menu = &debuggerMenu },
-        { "System configuration...", MENU, .menu = &sysconfigMenu },
-        { "Screen filters...", MENU, .menu = &screenFiltersMenu },
+        { "Lista processi", METHOD, .method = &RosalinaMenu_ProcessList },
+        { "Opzioni di debug...", MENU, .menu = &debuggerMenu },
+        { "Configurazione sistema...", MENU, .menu = &sysconfigMenu },
+        { "Filtri schermo...", MENU, .menu = &screenFiltersMenu },
         { "New 3DS menu...", MENU, .menu = &N3DSMenu, .visibility = &menuCheckN3ds },
-        { "Miscellaneous options...", MENU, .menu = &miscellaneousMenu },
-        { "Save settings", METHOD, .method = &RosalinaMenu_SaveSettings },
-        { "Power off", METHOD, .method = &RosalinaMenu_PowerOff },
-        { "Reboot", METHOD, .method = &RosalinaMenu_Reboot },
-        { "Credits", METHOD, .method = &RosalinaMenu_ShowCredits },
-        { "Debug info", METHOD, .method = &RosalinaMenu_ShowDebugInfo, .visibility = &rosalinaMenuShouldShowDebugInfo },
+        { "Altre opzioni...", MENU, .menu = &miscellaneousMenu },
+        { "Salva impostazioni", METHOD, .method = &RosalinaMenu_SaveSettings },
+        { "Spegni", METHOD, .method = &RosalinaMenu_PowerOff },
+        { "Riavvia", METHOD, .method = &RosalinaMenu_Reboot },
+        { "Crediti", METHOD, .method = &RosalinaMenu_ShowCredits },
+        { "Info debug", METHOD, .method = &RosalinaMenu_ShowDebugInfo, .visibility = &rosalinaMenuShouldShowDebugInfo },
         {},
     }
 };
@@ -85,11 +85,11 @@ void RosalinaMenu_SaveSettings(void)
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Save settings");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Salva impostazioni");
         if(R_SUCCEEDED(res))
-            Draw_DrawString(10, 30, COLOR_WHITE, "Operation succeeded.");
+            Draw_DrawString(10, 30, COLOR_WHITE, "Operazione effettuata.");
         else
-            Draw_DrawFormattedString(10, 30, COLOR_WHITE, "Operation failed (0x%08lx).", res);
+            Draw_DrawFormattedString(10, 30, COLOR_WHITE, "Operazione fallita (0x%08lx).", res);
         Draw_FlushFramebuffer();
         Draw_Unlock();
     }
@@ -117,18 +117,18 @@ void RosalinaMenu_ShowDebugInfo(void)
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Rosalina -- Debug info");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Rosalina -- Info debug");
 
         u32 posY = Draw_DrawString(10, 30, COLOR_WHITE, memoryMap);
         posY = Draw_DrawFormattedString(10, posY, COLOR_WHITE, "Kernel ext PA: %08lx - %08lx\n\n", kextPa, kextPa + kextSize);
         posY = Draw_DrawFormattedString(
-            10, posY, COLOR_WHITE, "Kernel version: %lu.%lu-%lu\n",
+            10, posY, COLOR_WHITE, "Versione kernel: %lu.%lu-%lu\n",
             GET_VERSION_MAJOR(kernelVer), GET_VERSION_MINOR(kernelVer), GET_VERSION_REVISION(kernelVer)
         );
         if (mcuFwVersion != 0)
         {
             posY = Draw_DrawFormattedString(
-                10, posY, COLOR_WHITE, "MCU FW version: %lu.%lu\n",
+                10, posY, COLOR_WHITE, "Versione MCU FW: %lu.%lu\n",
                 GET_VERSION_MAJOR(mcuFwVersion), GET_VERSION_MINOR(mcuFwVersion)
             );
         }
@@ -137,7 +137,7 @@ void RosalinaMenu_ShowDebugInfo(void)
         {
             u32 clkDiv = 1 << (1 + (speedInfo.sdClkCtrl & 0xFF));
             posY = Draw_DrawFormattedString(
-                10, posY, COLOR_WHITE, "SDMC speed: HS=%d %lukHz\n",
+                10, posY, COLOR_WHITE, "Velocita SDMC: HS=%d %lukHz\n",
                 (int)speedInfo.highSpeedModeEnabled, SYSCLOCK_SDMMC / (1000 * clkDiv)
             );
         }
@@ -145,7 +145,7 @@ void RosalinaMenu_ShowDebugInfo(void)
         {
             u32 clkDiv = 1 << (1 + (speedInfo.sdClkCtrl & 0xFF));
             posY = Draw_DrawFormattedString(
-                10, posY, COLOR_WHITE, "NAND speed: HS=%d %lukHz\n",
+                10, posY, COLOR_WHITE, "Velocita NAND: HS=%d %lukHz\n",
                 (int)speedInfo.highSpeedModeEnabled, SYSCLOCK_SDMMC / (1000 * clkDiv)
             );
         }
@@ -171,10 +171,11 @@ void RosalinaMenu_ShowCredits(void)
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Rosalina -- Luma3DS credits");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Rosalina -- Crediti Luma3DS");
 
         u32 posY = Draw_DrawString(10, 30, COLOR_WHITE, "Luma3DS (c) 2016-2023 AuroraWright, TuxSH") + SPACING_Y;
 
+        posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "Traduzione ITA by M4nueleZ");
         posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "3DSX loading code by fincs");
         posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "Networking code & basic GDB functionality by Stary");
         posY = Draw_DrawString(10, posY + SPACING_Y, COLOR_WHITE, "InputRedirection by Stary (PoC by ShinyQuagsire)");
@@ -183,10 +184,10 @@ void RosalinaMenu_ShowCredits(void)
 
         Draw_DrawString(10, posY, COLOR_WHITE,
             (
-                "Special thanks to:\n"
+                "Ringraziamenti speciali:\n"
                 "  fincs, WinterMute, mtheall, piepie62,\n"
-                "  Luma3DS contributors, libctru contributors,\n"
-                "  other people"
+                "  Contributori Luma3DS, contributori libctru,\n"
+                "  altre persone"
             ));
 
         Draw_FlushFramebuffer();
@@ -205,8 +206,8 @@ void RosalinaMenu_Reboot(void)
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Reboot");
-        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to reboot, press B to go back.");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Riavvia");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Premi A per riavviare, premi B per annullare.");
         Draw_FlushFramebuffer();
         Draw_Unlock();
 
@@ -239,23 +240,23 @@ void RosalinaMenu_ChangeScreenBrightness(void)
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Screen brightness");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Luminosita schermo");
         u32 posY = 30;
         posY = Draw_DrawFormattedString(
             10,
             posY,
             COLOR_WHITE,
-            "Current luminance: %lu (min. %lu, max. %lu)\n\n",
+            "Luminosita attuale: %lu (min. %lu, max. %lu)\n\n",
             luminance,
             minLum,
             maxLum
         );
-        posY = Draw_DrawString(10, posY, COLOR_WHITE, "Controls: Up/Down for +-1, Right/Left for +-10.\n");
-        posY = Draw_DrawString(10, posY, COLOR_WHITE, "Press A to start, B to exit.\n\n");
+        posY = Draw_DrawString(10, posY, COLOR_WHITE, "Controlli: Su/Giu per +-1, Destra/sinistra per +-10.\n");
+        posY = Draw_DrawString(10, posY, COLOR_WHITE, "Premi A per iniziare, B per uscire.\n\n");
 
-        posY = Draw_DrawString(10, posY, COLOR_RED, "WARNING: \n");
-        posY = Draw_DrawString(10, posY, COLOR_WHITE, "  * value will be limited by the presets.\n");
-        posY = Draw_DrawString(10, posY, COLOR_WHITE, "  * bottom framebuffer will be restored until\nyou exit.");
+        posY = Draw_DrawString(10, posY, COLOR_RED, "ATTENZIONE: \n");
+        posY = Draw_DrawString(10, posY, COLOR_WHITE, "  * valore verra limitato dai predefiniti.\n");
+        posY = Draw_DrawString(10, posY, COLOR_WHITE, "  * framebuffer inferiore verra ripristinato\nfino all'uscita.");
         Draw_FlushFramebuffer();
         Draw_Unlock();
 
@@ -332,8 +333,8 @@ void RosalinaMenu_PowerOff(void) // Soft shutdown.
     do
     {
         Draw_Lock();
-        Draw_DrawString(10, 10, COLOR_TITLE, "Power off");
-        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to power off, press B to go back.");
+        Draw_DrawString(10, 10, COLOR_TITLE, "Spegni");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Premi A per spegnere, premi B per annullare.");
         Draw_FlushFramebuffer();
         Draw_Unlock();
 
@@ -474,15 +475,15 @@ end:
         Draw_Lock();
         Draw_DrawString(10, 10, COLOR_TITLE, "Screenshot");
         if(R_FAILED(res))
-            Draw_DrawFormattedString(10, 30, COLOR_WHITE, "Operation failed (0x%08lx).", (u32)res);
+            Draw_DrawFormattedString(10, 30, COLOR_WHITE, "Operazione fallita (0x%08lx).", (u32)res);
         else
         {
             u32 t1 = (u32)(1000 * timeSpentConvertingScreenshot / SYSCLOCK_ARM11);
             u32 t2 = (u32)(1000 * timeSpentWritingScreenshot / SYSCLOCK_ARM11);
             u32 posY = 30;
-            posY = Draw_DrawString(10, posY, COLOR_WHITE, "Operation succeeded.\n\n");
-            posY = Draw_DrawFormattedString(10, posY, COLOR_WHITE, "Time spent converting:    %5lums\n", t1);
-            posY = Draw_DrawFormattedString(10, posY, COLOR_WHITE, "Time spent writing files: %5lums\n", t2);
+            posY = Draw_DrawString(10, posY, COLOR_WHITE, "Operazione effettuata.\n\n");
+            posY = Draw_DrawFormattedString(10, posY, COLOR_WHITE, "Tempo di conversione:    %5lums\n", t1);
+            posY = Draw_DrawFormattedString(10, posY, COLOR_WHITE, "Tempo di scrittura file: %5lums\n", t2);
         }
 
         Draw_FlushFramebuffer();
